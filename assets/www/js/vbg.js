5c1a589f8452ea81535502112132d204a3156b75
cklocker <christoph@wantsa.com>
2010-10-25 11:10:19 +0200

    added more sources

--- assets/www/js/vbg.js
+++ assets/www/js/vbg.js
@@ -9,4 +9,27 @@ function initList() {
 }
 
 
+$(document).ready(function () {
+    //getFeed('http://www.vol.at/rss.aspx/page/vol-article-index-rss-page/dc/tp:vol:vorarlberg');
+    localStorage.setItem("news-source", 'http://www.vol.at/rss.aspx/page/vol-article-index-rss-page/dc/tp:vol:musik-cds');
+    $('#feed-list').bind('pageAnimationStart', function(event, info){
+        $('#feed').hide();
+        $('#ajax-loader').show();
+        source = localStorage.getItem("news-source");
+        $('#feed').rssfeed(source, {
+            limit: 10
+        }, initList);
 
+    });
+
+    $('#feed-list').bind('pageAnimationEnd', function(event, info){
+        $('#feed').show();//
+    });
+
+
+    // sets id for news category
+    $('.news-source').delegate('a', 'click', (function(e){
+        source = localStorage.setItem("news-source", this.getAttribute("data-src"));
+    }));
+});
+                      
